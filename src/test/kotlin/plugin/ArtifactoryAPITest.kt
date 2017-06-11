6b4812b5968702e4288c1a04c6e586fbec195d80
Maxim <maximveligan@gmail.com>
2017-06-10 19:54:42 -0700

    Fixed bug, added download method, tested upload and download methods

--- src/test/kotlin/plugin/ArtifactoryAPITest.kt
+++ src/test/kotlin/plugin/ArtifactoryAPITest.kt
@@ -9,16 +9,29 @@ import org.jfrog.artifactory.client.*
 import org.junit.Assert.*
 import org.junit.Test
 import java.io.File
+import org.apache.commons.io.FileUtils.readFileToByteArray
 
-var artifactory = ArtifactoryClient.create("http://localhost:8081/artifactory/", "admin", "password")
-var path = "testFiles/"
-var testFile = File(path + "test1")
-
-var testRepository = "example-repo-local"
+val artifactory = ArtifactoryClient.create("http://localhost:8081/artifactory/", "admin", "password")
+val path = "testFiles/"
+val fileName = "test1"
+val testFile = File(path + fileName)
+val testRepository = "example-repo-local"
 
 public class ArtifactoryAPITest {
     @Test
     fun testLoad() {
-        artifactory.repository(testRepository).upload(path , testFile).doUpload();
+        artifactory.repository(testRepository).upload(path + fileName , testFile).doUpload();
+        val downloadedArtifact = artifactory.repository(testRepository).download(path + fileName).doDownload();
+        val expectedByteArray = readFileToByteArray(testFile)
+        val numberOfBytes = testFile.length()
+        var actualByteArray: ByteArray
+        var x = 0
+        while (x < numberOfBytes) {
+            assertTrue(downloadedArtifact.read().toByte() == expectedByteArray[x])
+            x++
+        }
+//        Lots of room for optimization here. Rewrite code to use assertArrayEquals, and get read(Byte[]) working
+//        with inputStream
     }
 }
+
