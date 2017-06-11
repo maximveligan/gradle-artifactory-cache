ac9b8e3bc8c741a8281102703d6b7d3c4d6bc7a3
Maxim <maximveligan@gmail.com>
2017-06-11 13:50:11 -0700

    Cleaned up repository and added .ignore file

---
+++ src/main/kotlin/plugin/artifactoryBuildCacheServices/ArtifactoryBuildCacheService.kt
@@ -0,0 +1,7 @@
+package plugin.artifactoryBuildCacheServices
+
+/**
+ * Created by grodion on 6/11/17.
+ */
+
+//TODO: Implement load(), store(), getDescription(), and close() from BuildCacheService
---
+++ src/main/kotlin/plugin/artifactoryBuildCacheServices/ArtifactoryBuildCacheServiceFactory.kt
@@ -0,0 +1,7 @@
+package plugin.artifactoryBuildCacheServices
+
+/**
+ * Created by grodion on 6/11/17.
+ */
+
+//TODO: should implement BuildCacheFactory from gradle
